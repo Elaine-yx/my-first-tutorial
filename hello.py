45d0cd76f03356e449ec80af630c791f4057fa42
Elaine-yx <ylxie25@uwcchina.org>
2026-09-03 09:18:17 +0800

    lesson 1 finish

---
+++ hello.py
@@ -0,0 +1,30 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#           A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Bro"
+food = "pizza"
+email = "Bro123@fake.com"
+# print(f"You are {age} years old")
+# print(f"Hello {first_name}")
+# print(f"You like {food}")
+# print(f"Your email is: {email}")
+
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+
+# Boolean
+is_student = True
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are NOT a student")
+
