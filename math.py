216a3c74f476d1de6db9f9584c39b1b2006f86f1
Elaine-yx <ylxie25@uwcchina.org>
2026-09-04 14:40:07 +0800

    math lesson finish

---
+++ math.py
@@ -0,0 +1,40 @@
+# arithmetic operations
+''''''
+friends = 2
+
+# friends += 2
+# friends -= 2
+# friends *= 2
+# friends /= 2
+# friends **= 2
+
+friends %= 2
+print(friends)
+''''''
+
+# built-in function
+''''''
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(x,y,z)
+result = min(x,y,z)
+
+print(result)
+''''''
+
+# math module
+import math
+print(math.pi)
+print(math.e)
+result = math.sqrt(x)
+result = math.ceil(x)
+result = math.floor(x)
+
+print(result)
+
+ 
