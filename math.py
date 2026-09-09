44521563057b6138c2d2b9104938924ec18e99b6
Elaine-yx <ylxie25@uwcchina.org>
2026-09-09 18:40:10 +0800

    finish lesson 4

--- math.py
+++ math.py
@@ -28,13 +28,36 @@ print(result)
 ''''''
 
 # math module
+''''''
 import math
-print(math.pi)
-print(math.e)
-result = math.sqrt(x)
-result = math.ceil(x)
-result = math.floor(x)
+
+x = 9.9
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)
+# result = math.floor(x)
 
 print(result)
+''''''
 
- 
+# circumference
+
+import math
+radius = float(input('Enter the radius of a circle: '))
+
+circumference = 2 * math.pi * radius
+print(f"The circumference is: {round.circumferencce, 2}cm")
+
+# area
+area = math.pi * pow(radius, 2)
+print(f"The area of the circle is: {round(area,2)}cm^2")
+
+# hypotenuse
+a = float(input("Enter side A: "))
+b = float(input("Enter side B: "))
+
+c = math.sqrt(pow(a, 2)) + pow(b, 2)
+
+print(f"Side C = {c}")
